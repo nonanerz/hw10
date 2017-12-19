b16506ea030d36b95197c9c1e296490d715d9efe
Bulava Eduard <bulavaeduard@gmail.com>
2017-12-19 16:17:15 +0200

    init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, TextInput, TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
 import ImagePicker from 'react-native-image-picker'
+import RNFS from 'react-native-fs'
 
 export default class Header extends React.Component {
     constructor (props) {
@@ -11,7 +12,41 @@ export default class Header extends React.Component {
         this.selectImage = this.selectImage.bind(this)
     }
     selectImage () {
+        let options = {
+            title: 'Select Avatar',
+            storageOptions: {
+                skipBackup: true,
+                path: 'images'
+            }
+        };
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response)
 
+            if (response.didCancel) {
+                console.log('User cancelled image picker')
+            }
+            else if (response.error) {
+                console.log('ImagePicker Error: ', response.error)
+            }
+            else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton)
+            }
+            else {
+                let filePath = RNFS.DocumentDirectoryPath + '/test-image.jpeg'
+                console.log('response', response)
+                RNFS.writeFile(filePath, response.data, 'base64')
+                    .then(() => {
+                        console.log('saved file', filePath)
+                    })
+                    .catch(err => {
+                        console.log('error save file', err)
+                    })
+                this.setState({
+                    avatarSource: { uri: response.uri }
+                    // avatarSource: { uri: 'file://' + filePath }
+                }, () => { console.log('avatar', this.state.avatarSource) })
+            }
+        })
     }
     render() {
         return (
